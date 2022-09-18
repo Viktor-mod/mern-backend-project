9bdc5aa6bef740de96d664664880c7086915e1ab
Viktor Modonov <modovi1998@gmail.com>
2022-09-18 17:21:05 +0800

    first commit

---
+++ validation.js
@@ -0,0 +1,21 @@
+import { body } from 'express-validator'
+
+export const loginValidation = [
+    body('email', 'Неверный формат почты').isEmail(),
+    body('password', 'Пароль должен быть минимум 5 символов').isLength({ min: 5 }),
+];
+
+export const registerValidation = [
+    body('email', 'Неверный формат почты').isEmail(),
+    body('password', 'Пароль должен быть минимум 5 символов').isLength({ min: 5 }),
+    body('firstName', 'Укажите имя').isLength({ min: 2 }),
+    body('lastName', 'Укажите фамилию').isLength({ min: 2 }),
+    body('avatarUrl', 'Неверная ссылка на аватар').optional().isURL(),
+];
+
+export const postCreateValidation = [
+    body('title', 'Введите заголовок статьи').isLength({ min: 3 }).isString(),
+    body('text', 'Введите текст статьи').isLength({ min: 3 }).isString(),
+    body('tags', 'Неверный формат тэгов (укажите массив)').optional().isString(),
+    body('imageUrl', 'Неверная ссылка на изображение').optional().isString(),
+];
